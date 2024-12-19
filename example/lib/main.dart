c625970ab8e3e1ba17514086fd5ce6e5522d1a7d
Mark <marksonline247@gmail.com>
2024-12-19 01:54:34 +0000

    edited example/pubspec file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:async_overlay/async_overlay.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,28 +18,35 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      home: const ExampleScreen(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+class ExampleScreen extends StatefulWidget {
+  const ExampleScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<ExampleScreen> createState() => _ExampleScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _ExampleScreenState extends State<ExampleScreen> {
+  final Calculator _calculator = Calculator();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Home'),
+        title: Text('Example'),
       ),
       body: Center(
-        child: Text('Testing the package'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('${_calculator.addOne(7)}'),
+          ],
+        ),
       ),
     );
   }
